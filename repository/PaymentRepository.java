955157f3096472637a4f930f54b6ee25d2dd6d7a
Jitendra Kumar Sharma <136931188+Jitendra-Sharma726@users.noreply.github.com>
2026-06-17 16:33:56 +0530

    Create PaymentRepository interface for payment data

---
+++ repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import entity.PaymentInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<PaymentInfo, Long> {
+}
